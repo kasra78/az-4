45e4988771dcb6fa729ec41f2bdce98887bad084
kasra78 <damavandi_kasra@yahoo.com>
2022-11-28 14:15:54 +0330

    Add class Square

---
+++ src/main/java/Square.java
@@ -0,0 +1,19 @@
+public class Square {
+    int length;
+
+    public Square(int length) {
+        this.length = length;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    int computeArea() {
+        return this.length ^ 2;
+    }
+}
